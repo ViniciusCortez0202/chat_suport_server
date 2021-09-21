1cbf4063e12a77176bf135f209806f6951a75c29
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-09-21 20:35:39 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+var readline = require('readline');
+var dateTime = new Date();
+
+
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var res = "";
+
+var chamados = [
+
+]
+
+io.on('connection', (socket) => { 
+
+    console.log(socket.id);
+
+    socket.on("enterpriseInformation", (data) => {
+        console.log(socket.id);
+        console.log(data.enterpriseID);
+        chamados.map((e) => {
+            if(e.id = data.idCall) return e.status = "Ativo";
+        })
+    })
+
+    socket.on("sender", (data) => {
+        leitor.question(`Resposta: ${data.text}\n`, function(answer) {
+            var resp = answer;
+            socket.emit('response', {
+                isMe: false,
+                text: resp,
+                time: `${dateTime.getHours()}:${dateTime.getMinutes() < 10 ? '0'+dateTime.getMinutes() : dateTime.getMinutes()}`,
+                unread: false
+            })            
+        });
+    });
+ });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+ app.use('/chamados', (request, response) => {
+    var body = request.body;
+    var novoChamado = {
+        id: dateTime.getMilliseconds(),
+        entepriseId: body.entepriseId,
+        motivo: body.motivo,
+        service: body.servico,
+        status: body.status,
+        descricao: body.descricao,
+        data: `${dateTime.getDay()}/${dateTime.getMonth()}/${dateTime.getFullYear()}`
+    }
+    chamados.push(novoChamado);
+    console.log(chamados)
+    response.send().status(200);
+});
+
+app.use('/getChamados', (request, response) => {
+    console.log(chamados);
+    response.status(200).json(chamados);
+});
+server.listen(5000, '10.0.0.104');
