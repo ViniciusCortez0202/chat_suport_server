8cccd3b83fd9a67b7c94919e657a22198e770cc9
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-10-01 10:19:40 -0300

    created enums type and status

---
+++ socketsSuportChat/enums/statusCall.js
@@ -0,0 +1,7 @@
+const Status = Object.freeze({
+    OPEN:   { name: "Aberto", color: "" },
+    ACTIVATE:  { name: "Aberto", color: "" },
+    CLOSE: {name: "Fechado", color: ""}
+  });
+
+module.exports = Status;
---
+++ socketsSuportChat/enums/typesChat.js
@@ -0,0 +1,6 @@
+const Type = Object.freeze({
+    ENTERPRISE:   { id: "1" },
+    SUPORT:  { id: "2" },
+  });
+
+module.exports = Type;
