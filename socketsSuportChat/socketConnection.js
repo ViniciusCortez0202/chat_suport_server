926f54163b4b24b55b1888cb9a2bb87e1e2f94a2
Vinicius_Cortez <cortezvinicius11@gmail.com>
2021-10-02 11:08:31 -0300

    add type user on message

--- socketsSuportChat/socketConnection.js
+++ socketsSuportChat/socketConnection.js
@@ -10,6 +10,7 @@ chatSuport = (io) => {
             let message;
             if (data.type == Type.ENTERPRISE.id) {
                 message = {
+                    type: Type.ENTERPRISE,
                     idUser: data.idUser,
                     codeEnterprise: data.codeEnterprise,
                     idCall: data.idCall,
@@ -17,6 +18,7 @@ chatSuport = (io) => {
                 }
             } else if (data.type == Type.SUPORT.id) {
                 message = {
+                    type: Type.SUPORT,
                     idUser: data.idUser,
                     idCall: data.idCall,
                     message: data.message,
@@ -24,19 +26,21 @@ chatSuport = (io) => {
             }
             socket.join(message.idCall);
             controller.insertMessage(message);
-            sendMessage(message);
+            controller.sendMessage(message);
         });
 
         socket.on("auth", (data) => {
             let model;
             if (data.type == Type.ENTERPRISE.id) {
                 model = {
+                    type: Type.ENTERPRISE,
                     codeEnterprise: data.idEnterprise,
                     idUser: data.idUser,
                     socketID: socket.id
                 }
             } else if (data.type == Type.SUPORT.id) {
                 model = {
+                    type: Type.SUPORT,
                     isUser: data.idUser,
                     socketID: socket.id
                 }
