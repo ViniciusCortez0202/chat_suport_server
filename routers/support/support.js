339536e1e5183aa6c5cdf6621da8b4d8acd1ef76
= <cortezvinicius11@gmail.com>
2022-11-14 14:17:14 -0300

    close call

--- routers/support/support.js
+++ routers/support/support.js
@@ -1,12 +1,13 @@
-import { response } from 'express';
 import Authentication from '../../authentication/auth.js';
 import SupportData from '../../data/support.js';
+import SupporCallstData from '../../data/support_calls.js';
 import connection from './../../data/connection.js';
 
 const supportRouters = (router) => {
 
     const support = new SupportData(connection);
-    const auth  = new Authentication(support)
+    const supportCalls = new SupporCallstData(connection);
+    const auth = new Authentication(support)
 
     router.get('/all', async (require, response, next) => {
         try {
@@ -17,23 +18,40 @@ const supportRouters = (router) => {
         }
     });
 
-    router.put("/call/:id", auth.auth, async (require, response) => {
-        const assignment = {
-            support_id: response.locals.userId,
-            calls_id: parseInt(require.params.id)
-        }
+    router.route("/call/:id")
+        .all(auth.auth)
+        .post(async (require, response) => {
 
-        try {
+            const assignment = {
+                support_id: response.locals.userId,
+                calls_id: parseInt(require.params.id)
+            }
 
-            const result = await support.insertCallToSupport(assignment);
-            
-            response.status(200).send({message: "Chamado atribuido"});
-            
-        } catch (error) {
-            response.status(400).send({message: "Chamado não encontrado"}); 
-        }
+            try {
+                const result = await supportCalls.insertCallToSupport(assignment);
 
-    })
+                response.status(200).send({ message: "Chamado atribuido" });
+            } catch (error) {
+                response.status(400).send({ message: "Chamado não encontrado" });
+            }
+        })
+        .delete(async (require, response) => {
+
+            const idSupport = response.locals.userId;
+            const idCall = parseInt(require.params.id);
+
+            try {
+                await supportCalls.verifyCallFromSupportAndReturnExceptionIfNot(idSupport, idCall);
+                await supportCalls.updateStatusCall("Fechado", idCall);
+                
+                response.status(200).send({ message: "Chamado Fechado" });
+
+            } catch (error) {
+                console.log(error)
+                response.status(400).send({ message: "Chamado não encontrado" });
+            }
+
+        })
 
     return router;
 }
