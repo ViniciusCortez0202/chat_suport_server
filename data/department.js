d7ad1304f0f1959ae78e0bd076514e9aaf9c1aad
= <cortezvinicius11@gmail.com>
2022-11-16 11:46:02 -0300

    departament data

--- data/department.js
+++ data/department.js
@@ -1,21 +1,34 @@
-import { query as _query } from './connection';
-
 class Departament{
 
-    selectDepartment = () => {
-        const query = "SELECT * FROM department";
+    connection;
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    select = () => {
+        const query = "SELECT * FROM departament";
         return new Promise((resolve, reject) => {
-            _query(query, (err, result) => {
+            this.connection.query(query, (err, result) => {
                 if(err) return reject(err);
                 return resolve(result);
             })
         });
     }
 
-    insertDepartment = (values) => {
-        const query = "INSERT INTO department SET ?";
+    insert = (values) => {
+        const query = "INSERT INTO departament SET ?";
         return new Promise((resolve, reject) => {
-            _query(query, values, (err, result) => {
+            this.connection.query(query, values, (err, result) => {
+                if(err) return reject(err);
+                return resolve(result);
+            })
+        });
+    }
+
+    selectClientById = (id) => {
+        const query = "SELECT * FROM departament WHERE id = ?";
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, id, (err, result) => {
                 if(err) return reject(err);
                 return resolve(result);
             })
